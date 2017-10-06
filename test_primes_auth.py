42a8fa55d796e4c67501bc4fdb101ecaf08a7481
Steve Saporta <steve@swipetospin.com>
2017-10-05 20:33:45 -0400

    Added pytest.raises example

--- test_primes_auth.py
+++ test_primes_auth.py
@@ -2,6 +2,8 @@
 Defines unit tests for the "primes" module, with authentication.
 """
 
+import pytest
+
 from primes_auth import is_prime, prime_factors, User
 
 
@@ -16,3 +18,16 @@ def test_prime_factors():
     user = User('python', 'rocks')
     assert prime_factors(user, 2) == [2]
     assert prime_factors(user, 12) == [2, 2, 3]
+
+
+def test_pytest_raises():
+    user = User('python', 'bogus')
+    with pytest.raises(RuntimeError):
+        prime_factors(user, 2)
+
+    # Equivalent to:
+    try:
+        prime_factors(user, 2)
+        raise Exception('Failed to raise RuntimeError')
+    except RuntimeError:
+        pass
